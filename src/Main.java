29080d678e65942c552f2e73ad783870ecacd62b
L3me05 <l.dimarcantonio@elitesoftwarehouse.com>
2025-04-07 11:32:04 +0200

    Algoritmi fino all'11

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         //algortimo che vuoi testare da 1-10
-        algoritmo_10();
+        algoritmo_11();
 
     }
 
@@ -163,15 +163,15 @@ public class Main {
             f++;
         }
 
-        int i=0;
-        boolean rip=false;
-        if (ar.length>2) {
+        int i = 0;
+        boolean rip = false;
+        if (ar.length > 2) {
             do {
-                int j=i+1;
-                while ( j<ar.length && ar[j]!=ar[i]) j++;
+                int j = i + 1;
+                while (j < ar.length && ar[j] != ar[i]) j++;
                 i++;
-                if(j!=ar.length) rip=true;
-            } while (i<ar.length-1 && !rip);
+                if (j != ar.length) rip = true;
+            } while (i < ar.length - 1 && !rip);
         }
 
         if (rip) System.out.println("Ci sono caratteri ripetuti");
@@ -180,47 +180,47 @@ public class Main {
 
     private static void algoritmo_9() {
         //Dato un array ordinato in ordine crescente determina se ci sono numeri ripetuti nel caso in cui stampa a video quali e quanti
-            //caricamento vettore con controllo inserimento solo numeri crescenti
+        //caricamento vettore con controllo inserimento solo numeri crescenti
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserisci la grandezza dell'array");
         int[] ar = new int[scanner.nextInt()];
-        int f=0;
-        while (f<ar.length) {
+        int f = 0;
+        while (f < ar.length) {
             System.out.println("Inserisci un numero: ");
-            ar[f]=scanner.nextInt();
-            while (f!=0 && ar[f]<ar[f-1]) {
+            ar[f] = scanner.nextInt();
+            while (f != 0 && ar[f] < ar[f - 1]) {
                 System.out.println("Hai inserito un numero minore del precendente, l'array deve essere crescente \n Inserisci un altro valore ");
-                ar[f]=scanner.nextInt();
+                ar[f] = scanner.nextInt();
             }
             f++;
         }
 
         //corpo algoritmo
-        int i=0;
-        int k=0;
-        int[] ar2 = new int[ar.length/2];       //inserisco i numeri ripetuti
-        int[] ar3 = new int[ar.length/2];       //nella stessa posizione di ar2 inserisco quante vole sono ripetuti
-        while (i<ar.length-1) {
-            int j=i+1;
-            while (j<ar.length && ar[j]==ar[i]) j++;
-            if(j-i>1) {
-                ar2[k]=ar[i];
-                ar3[k]=j-i-1;
+        int i = 0;
+        int k = 0;
+        int[] ar2 = new int[ar.length / 2];       //inserisco i numeri ripetuti
+        int[] ar3 = new int[ar.length / 2];       //nella stessa posizione di ar2 inserisco quante vole sono ripetuti
+        while (i < ar.length - 1) {
+            int j = i + 1;
+            while (j < ar.length && ar[j] == ar[i]) j++;
+            if (j - i > 1) {
+                ar2[k] = ar[i];
+                ar3[k] = j - i - 1;
                 k++;
             }
-            i=j;
+            i = j;
 
         }
 
         //stampa risultati
-        i=0;
-        int totRip=0;
-        while (i<k) {
-            System.out.println(ar2[i]+" è un numero ripetuto \n Si ripete: "+ar3[i]+" volte");
-            totRip+=ar3[i];
+        i = 0;
+        int totRip = 0;
+        while (i < k) {
+            System.out.println(ar2[i] + " è un numero ripetuto \n Si ripete: " + ar3[i] + " volte");
+            totRip += ar3[i];
             i++;
         }
-        System.out.println("In totale ci sono "+totRip+" ripetizioni");
+        System.out.println("In totale ci sono " + totRip + " ripetizioni");
 
 
     }
@@ -231,37 +231,73 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Inserisci la grandezza del primo array");
         int[] ar = new int[scanner.nextInt()];
-        int f=0;
-        while (f<ar.length) {
+        int f = 0;
+        while (f < ar.length) {
             System.out.println("Inserisci un numero: ");
-            ar[f]=scanner.nextInt();
+            ar[f] = scanner.nextInt();
             f++;
         }
 
         //caricamento vettore
         System.out.println("Inserisci la grandezza del secondo array");
         int[] ar2 = new int[scanner.nextInt()];
-        f=0;
-        while (f<ar2.length) {
+        f = 0;
+        while (f < ar2.length) {
             System.out.println("Inserisci un numero: ");
-            ar2[f]=scanner.nextInt();
+            ar2[f] = scanner.nextInt();
             f++;
         }
 
-        if (ar.length==ar2.length) {
-            int prodottoScalare=0;
-            int [] ar3 = new int[ar.length];
-            int i=0;
-            while (i<ar.length) {
-                ar3[i]=ar[i]*ar2[i];
-                prodottoScalare+=ar3[i];
+        if (ar.length == ar2.length) {
+            int prodottoScalare = 0;
+            int[] ar3 = new int[ar.length];
+            int i = 0;
+            while (i < ar.length) {
+                ar3[i] = ar[i] * ar2[i];
+                prodottoScalare += ar3[i];
                 i++;
             }
-            System.out.println("Il prodotto scalare è " + prodottoScalare );
-        }
-        else System.out.println("Il prodotto scalare non si può fare in quanto gli array sono di dimensioni diverse");
+            System.out.println("Il prodotto scalare è " + prodottoScalare);
+        } else System.out.println("Il prodotto scalare non si può fare in quanto gli array sono di dimensioni diverse");
     }
 
+    private static void algoritmo_11() {
+        //Somma i valori contenuti in una matrice e stampa a video
+
+        //caricamento matrice
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci il numero di righe della matrice:");
+        int n = scanner.nextInt();
+        System.out.println("Inserisci il numero delle colonne della matrice");
+        int m = scanner.nextInt();
+        int[][] mat = new int[n][m];
+
+        //corpo algoritmo
+        int f = 0;
+        while (f < n) {
+            int g = 0;
+            while (g < m) {
+                System.out.println("Inserisci un numero da inserire nella riga " + f);
+                mat[f][g] = scanner.nextInt();
+                g++;
+            }
+            f++;
+        }
+
+        int somma=0;
+        int i=0;
+        while (i<mat.length) {
+            int j=0;
+            while (j<mat[0].length) {
+                somma+=mat[i][j];
+                j++;
+            }
+            i++;
+        }
+
+        System.out.println("La somma dei valori della matrice è: " + somma);
+
+    }
 
 
 }
