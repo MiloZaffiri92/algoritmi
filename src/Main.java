8d77a905fd0bb24cd7ea942163af22d9fe5191bb
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2025-04-09 10:59:13 +0200

    Sistemato branch

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,8 @@ public class Main {
         algoritmo19();
         algoritmo20();
         algoritmo21();
+        algoritmo22();
+        algoritmo23();
     }
 
     public static void algoritmo1() {
@@ -369,7 +371,7 @@ public class Main {
     }
 
     public static void algoritmo15() {
-        //Fai la somma dei valori contenuti in una matrice n x m
+        //Fai la sopmma dei valori contenuti in una matrice n x m
         Scanner scan = new Scanner(System.in);
         System.out.println("Quante righe vuoi che abbia la matrice?");
         int n = scan.nextInt();
@@ -547,7 +549,80 @@ public class Main {
         }
     }
 
+    public static void algoritmo22() {
+        //Realizzare un algoritmo da un vettore A disposto da numeri interi in posizione crescente aggiunga in un nuovo vettore B lungo n + 1 un nuovo numero messo nella posizione corretta
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Quanto vuoi che sia lungo il vettore A");
+        int n = scan.nextInt();
+        int[] A = new int[n];
+        int[] B = new int[n + 1];
+        int i = 0;
+        while (i < n) {
+            System.out.println("Che numero vuoi aggiungere al vettore in ordine crescente?");
+            A[i] = scan.nextInt();
+            i++;
+        }
+        i = 0;
+        int j = 0;
+        boolean inserito = false;
+        System.out.println("Che numero vuoi aggiungere nel vettore B?");
+        int num = scan.nextInt();
+        while (i < n) {
+            if (A[i] < num || inserito) {
+                B[j] = A[i];
+                i++;
+            } else {
+                B[j] = num;
+                inserito = true;
+            }
+            j++;
+        }
+        if (!inserito) {
+            B[j] = num;
+        }
+        j = 0;
+        System.out.print("Vettore B con numero aggiunto:");
+        while (j < n + 1) {
+            System.out.print(" " + B[j]);
+            j++;
+        }
+    }
 
-
+    public static void algoritmo23() {
+        //Dato un array di interi ed una target sum, trova gli indice dei 2 numeri che sommati risultano essere il target; diagramma di flusso, java, passo passo, pseudo
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Quanto vuoi che sia lungo il vettore");
+        int n = scan.nextInt();
+        int[] V = new int[n];
+        int i = 0;
+        while (i < n) {
+            System.out.println("Che numero vuoi aggiungere nel vettore in posizione " + i + "?");
+            V[i] = scan.nextInt();
+            i++;
+        }
+        i = 0;
+        System.out.println("Qual'è la somma di numeri che stai cercando?");
+        int targetsum = scan.nextInt();
+        boolean trovata = false;
+        int[] V2 = new int[2];
+        while (i < n - 1 && !trovata) {
+            int j = i + 1;
+            while (j < n && !trovata) {
+                int somma = V[i] + V[j];
+                if (somma == targetsum) {
+                    trovata = true;
+                    V2[0] = i;
+                    V2[1] = j;
+                }
+                j++;
+            }
+            i++;
+        }
+        if (trovata) {
+            System.out.println("La somma " + targetsum + " è stata trovata ed e composta dal numero in posizione " + V2[0] + " e " + V2[1] + " del vettore V, perchè " + V[V2[0]] + "+" + V[V2[1]] + "=" + targetsum);
+        } else {
+            System.out.println("La somma " + targetsum + " non è stata trovata nel vettore V");
+        }
+    }
 }
 
